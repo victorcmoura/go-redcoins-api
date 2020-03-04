6038f331a58a0d4dc1588ac36862082a13e88eca
Victor Moura <victor_cmoura@hotmail.com>
2020-03-04 10:05:51 -0300

    Removes seed

--- api/seed/seeder.go
+++ api/seed/seeder.go
@@ -1,70 +1,10 @@
 package seed
 
 import (
-	"log"
-	"time"
-
 	"github.com/jinzhu/gorm"
 	"github.com/victorcmoura/go-redcoins-api/api/models"
 )
 
-var users = []models.User{
-	models.User{
-		Name: "Victor Moura",
-		Email: "victor@email.com",
-		Password: "password",
-		BirthDate: time.Now().AddDate(-30, 0, 0),
-	},
-	models.User{
-		Name: "Moura Victor",
-		Email: "moura@email.com",
-		Password: "password",
-		BirthDate: time.Now().AddDate(-31, 0, 0),
-	},
-}
-
-var transactions = []models.Transaction{
-	models.Transaction{
-		Type: "buy",
-		BCValue: 1,
-		USDValue: -1,
-	},
-	models.Transaction{
-		Type: "buy",
-		BCValue: -1,
-		USDValue: 10000,
-	},
-}
-
 func Load(db *gorm.DB) {
-
-	err := db.Debug().DropTableIfExists(&models.Transaction{}, &models.User{}).Error
-	if err != nil {
-		log.Fatalf("cannot drop table: %v", err)
-	}
-	err = db.Debug().AutoMigrate(&models.User{}, &models.Transaction{}).Error
-	if err != nil {
-		log.Fatalf("cannot migrate table: %v", err)
-	}
-
-	err = db.Debug().Model(&models.Transaction{}).AddForeignKey("owner_id", "users(id)", "cascade", "cascade").Error
-	if err != nil {
-		log.Fatalf("attaching foreign key error: %v", err)
-	}
-
 	models.UpdatePrice(db)
-
-	for i, _ := range users {
-		err = db.Debug().Model(&models.User{}).Create(&users[i]).Error
-		if err != nil {
-			log.Fatalf("cannot seed users table: %v", err)
-		}
-		transactions[i].OwnerID = users[i].ID
-
-		transactions[i].Prepare(db)
-		_, err = transactions[i].SaveTransaction(db)
-		if err != nil {
-			log.Fatalf("cannot seed transactions table: %v", err)
-		}
-	}
 }
